c7d2b173f82ff6ab5f7d0348e8c4ade8cd98b704
ruthea <ruthea@usxxrutheam1.corp.emc.com>
2013-09-16 00:54:30 -0500

    updated

--- recipes/spring-ws-recipes-client/src/main/java/com/rollingstone/recipes/oxm/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://binit.blogspot.com/ws/schema/recipe", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package com.rollingstone.recipes.oxm;
